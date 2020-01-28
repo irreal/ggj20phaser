a3d6d8ccb1df774294dda846fffe5d401b032ce5
Miloš Spasojević <milos.s.pfc@gmail.com>
2020-01-28 23:19:12 +0100

    Added basic drawing mechanic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Phaser from "phaser";
 import buttonImg from "./assets/button.png";
+import blackSquareImg from "./assets/blacksquare.png";
+import whiteSquareImg from "./assets/whitesquare.png";
 import configuration from "./assets/configuration/configuration";
 
 const serverUrl = configuration.apiUrl;
@@ -7,8 +9,8 @@ const serverUrl = configuration.apiUrl;
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
-  width: 800,
-  height: 600,
+  width: 1400,
+  height: 700,
   scene: {
     preload: preload,
     create: create
@@ -19,21 +21,51 @@ const game = new Phaser.Game(config);
 
 function preload() {
   this.load.image("button", buttonImg);
+  this.load.image("blacksquare", blackSquareImg);
+  this.load.image("whitesquare", whiteSquareImg);
 }
 
+var squares = [];
 function create() {
-  const button = this.add.image(400, 150, "button");
+
+  this.cameras.main.setBackgroundColor('#FFFFFF')
+
+  const button = this.add.image(400, 50, "button");
   button.setInteractive();
+
+
+  for (var i = 0; i < 12; i++) {
+    squares.push([]);
+    for (var u = 0; u < 17; u++) {
+      const img = this.add.image(30 + u * 51, 100 + i * 51, "whitesquare");
+      img.setInteractive();
+      squares[i].push(img);
+
+    }
+  }
 
   this.input.on('gameobjectdown', (pointer, gameObject) => {
     if (gameObject === button) {
+      var data = [...squares].map(arr => {
+        return arr.map(i => i.texture.key == 'blacksquare' ? 1 : 0);
+      }
+      );
+      console.log('here is the data', data);
       fetch(serverUrl + 'event', {
         method: 'post',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ action: 'create' })
+        body: JSON.stringify({ action: 'img', data })
       })
+    }
+    else {
+      if (gameObject.texture.key == 'blacksquare') {
+        gameObject.setTexture('whitesquare');
+      }
+      else {
+        gameObject.setTexture('blacksquare');
+      }
     }
   });
 
